952967186855a6fdf617402f2b8982d8261ab3dc
luispedro <luispedro@ff509d18-21b0-403c-84e0-5236789433d0>
2008-10-15 19:59:42 +0000

    Fix error cache
    git-svn-id: file:///home/luispedro/svnroot/milk/trunk@7 ff509d18-21b0-403c-84e0-5236789433d0

--- milk/supervised/svm.py
+++ milk/supervised/svm.py
@@ -22,7 +22,6 @@
 from __future__ import division
 from numpy import vectorize
 from math import *
-from numpy import *
 import numpy
 import random
 
@@ -59,6 +58,10 @@ def svm_learn(X,Y,kernel,C,eps=1e-3,tol=1e-8):
                 sum -= .5*Y[i]*Y[j]*kernel(X[i],X[j])*Alphas[i]*Alphas[j]
         return sum
         
+    def get_error(i):
+        if Alphas[i] in (0,C):
+            return f_at(X[i])-Y[i]
+        return E[i]
     def take_step(i1,i2):
         if i1 == i2: return False
         alpha1 = Alphas[i1]
@@ -66,13 +69,8 @@ def svm_learn(X,Y,kernel,C,eps=1e-3,tol=1e-8):
         y1 = Y[i1]
         y2 = Y[i2]
         s = y1*y2
-        E1 = E[i1] 
-        E2 = E[i2] 
-        if  Alphas[i1] in (0,C):
-            E1 = f_at(X[i1])-y1
-        if Alphas[i2] in (0,C):
-            E2 = f_at(X[i2])-y2
-
+        E1 = get_error(i1)
+        E2 = get_error(i2)
         if y1 != y2:
             L = max(0,alpha2-alpha1)
             H = min(C,C+alpha2-alpha1)
@@ -88,11 +86,11 @@ def svm_learn(X,Y,kernel,C,eps=1e-3,tol=1e-8):
         eta = 2*k12-k11-k22
         if eta < 0:
             a2 = alpha2-y2*(E1-E2)/eta
-            a2 = median((L,a2,H))
+            a2 = numpy.median((L,a2,H))
         else:
             gamma = alpha1+s*alpha2 # Eq. (12.22)
-            v1=f_at(X[i1])+thresh[0]-y1*alpha1*k11-y2*alpha2*k12 # Eq. (12.21)
-            v2=f_at(X[i2])+thresh[0]-y1*alpha1*k12-y2*alpha2*k22 # Eq. (12.21)
+            v1=E1+y1+thresh[0]-y1*alpha1*k11-y2*alpha2*k12 # Eq. (12.21) # Note that f(x1) = E1 + y1
+            v2=E2+y2+thresh[0]-y1*alpha1*k12-y2*alpha2*k22 # Eq. (12.21)
             L_obj = gamma-s*L+L-.5*k11*(gamma-s*L)**2-.5*k22*L**2-s*k12*(gamma-s*L)*L-y1*(gamma-s*L)*v1-y2*L*v2 # + W_const # Eq. (12.23)
             H_obj = gamma-s*H+H-.5*k11*(gamma-s*H)**2-.5*k22*H**2-s*k12*(gamma-s*H)*H-y1*(gamma-s*H)*v1-y2*H*v2 # + W_const # Eq. (12.23)
             if L_obj > H_obj + eps:
@@ -105,7 +103,7 @@ def svm_learn(X,Y,kernel,C,eps=1e-3,tol=1e-8):
             a2 = 0
         elif a2 > C-tol:
             a2 = C
-        if abs(a2-alpha2) < eps*(a2+alpha2+eps): return False
+        if numpy.abs(a2-alpha2) < eps*(a2+alpha2+eps): return False
         a1 = alpha1+s*(alpha2-a2)
         if a1 < eps: a1 = 0
         if a1 > C-eps: a1 = C
@@ -117,22 +115,25 @@ def svm_learn(X,Y,kernel,C,eps=1e-3,tol=1e-8):
         b2 = E2 + Y[i1]*(a1-alpha1)*k12+Y[i2]*(a2-alpha2)*k22+thresh[0] # Eq. (12.10)
         new_b = (b1+b2)/2.
         for i in xrange(N):
-            if 0 < Alphas[i] < C:
+            if Alphas[i] in (0,C):
                 continue
             elif i == i1 or i == i2:
                 E[i] = 0
             else:
                 E[i] += y1*(a1-alpha1)*kernel(X[i1],X[i])+y2*(a2-alpha2)*kernel(X[i2],X[i]) + (thresh[0]-new_b) # Eq. (12.11)
         thresh[0] = new_b
+        E[i1]=f_at(X[i1])-y1
+        E[i2]=f_at(X[i2])-y2
         return True
     def examine_example(i2):
         y2 = Y[i2]
         alpha2 = Alphas[i2]
-        E2 = f_at(X[i2]) - y2
+        E2 = get_error(i2)
         r2 = E2 * y2
         #print 'alpha2', alpha2, 'E2', E2, 'r2', r2
         if (r2 < -tol) and (alpha2 < C) or (r2 > tol) and (alpha2 > 0):
             if Alphas.any() or (Alphas==C).any():
+                 dE = numpy.array([numpy.abs(get_error(i)-E2) for i in xrange(N)])
                  dE = numpy.array([numpy.abs(f_at(X[i])-Y[i]) for i in xrange(N)])
                  i1 = dE.argmax()
                  if take_step(i1,i2):
