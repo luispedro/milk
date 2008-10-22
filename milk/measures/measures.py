bd3f50e11160d5fc8edfd54e22c66f17ce2f5f4e
luispedro <luispedro@ff509d18-21b0-403c-84e0-5236789433d0>
2008-10-22 21:57:38 +0000

    Measure of algorithmic goodness
    git-svn-id: file:///home/luispedro/svnroot/milk/trunk@19 ff509d18-21b0-403c-84e0-5236789433d0

---
+++ milk/measures/measures.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008, Luís Pedro Coelho <lpc@cmu.edu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+from __future__ import division
+import numpy
+
+__all__ = ['accuracy','waccuracy']
+
+def accuracy(cmatrix):
+    '''
+    acc = accuracy(cmatrix)
+
+    Accuracy of confusion matrix
+    '''
+    cmatrix = numpy.asanyarray(cmatrix)
+    return cmatrix.trace()/cmatrix.sum()
+
+def waccuracy(cmatrix):
+    '''
+    wacc = waccuracy(cmatrix)
+
+    Weighted accuracy of cmatrix
+    '''
+    cmatrix = numpy.asanyarray(cmatrix)
+    return (cmatrix.diagonal() / cmatrix.sum(1)).mean()
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
+
