b4425cdd322bf648bfb1a77725cabf8df0d2d03e
Luís Pedro Coelho <lpc@cmu.edu>
2008-11-19 16:40:30 -0500

    Nearest Neighbour Classifier
    Naïve implementation of nearest neighbour classifier

---
+++ milk/supervised/nearestneighbour.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008, Luís Pedro Coelho <lpc@cmu.edu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+from __future__ import division
+import numpy
+
+__all__ = ['NNClassifier']
+
+class NNClassifier(object):
+    '''
+    Nearest Neighbour Classifier
+    ----------------------------
+
+    Implements a nearest neighbour classifier. Currently, uses the simplest
+    implementation with O(N) classification time.
+    '''
+    def __init__(self):
+        pass
+    
+    def train(self,features,labels):
+        self.training=features.copy()
+        self.labels=labels.copy()
+
+    def apply(self,feats):
+        dists = ((self.training-feats)**2).sum(1)
+        idx = dists.argmin()
+        return self.labels[idx]
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
