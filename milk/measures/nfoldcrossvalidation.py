0bad339b06c0b8e8fd71953f072e74dfe1a1f3e2
Luís Pedro Coelho <lpc@cmu.edu>
2008-11-19 16:57:01 -0500

    N-Fold Crossvalidation (from pyslic)

---
+++ milk/measures/nfoldcrossvalidation.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008, Luís Pedro Coelho <lpc@cmu.edu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+from __future__ import division
+import numpy
+from numpy import *
+from ..classify import defaultclassifier
+from ..classifier import normaliselabels
+
+__all__=['nfoldcrossvalidation']
+def nfoldcrossvalidation(features,labels,nfolds=None,classifier=None, return_predictions=False):
+    '''
+    Perform n-fold cross validation
+
+    cmatrix,labelnames = nfoldcrossvalidation(features, labels, nfolds=10, classifier=None, return_predictions=False)
+    cmatrix,labelnames,predictions = nfoldcrossvalidation(features, labels, nfolds=10, classifier=None, return_predictions=False)
+
+    cmatrix will be a N x N matrix, where N is the number of classes
+    cmatrix[i,j] will be the number of times that an element of class i was classified as class j
+
+    labelnames[i] will correspond to the label name of class i
+
+    Arguments
+    ---------
+        * features: a feature matrix or list of feature vectors
+        * labels: an array of labels, where label[i] is the label corresponding to features[i]
+        * nfolds: Nr of folds.
+        * return_predictions: whether to return predictions
+
+    classifier should implement the train() and apply() methods
+    '''
+    assert len(features) == len(labels), 'milk.measures.nfoldcrossvalidation: len(features) should match len(labels)'
+    if classifier is None:
+        classifier = defaultclassifier()
+    labels,labelnames=normaliselabels(labels)
+    predictions=labels*0-1
+
+    features = numpy.asanyarray(features)
+
+    classcounts={}
+    for L in labels:
+        classcounts[L] = classcounts.get(L,0) + 1
+
+    min_class_count = min(classcounts.values())
+    if nfolds is None:
+        nfolds = min(10,min_class_count)
+    elif min_class_count < nfolds:
+        from warnings import warn
+        warn('milk.measures.nfoldcrossvalidation: Reducing the nr. of folds to %s (minimum class size).' % min_class_count)
+        nfolds=min_class_count
+    
+    nclasses=len(classcounts)
+    cmatrix=zeros((nclasses,nclasses))
+    for fold in xrange(nfolds):
+        testingset=zeros(len(labels),bool)
+
+        for L,C in classcounts.items():
+            idxs,=where(labels==L)
+            N=len(idxs)
+            perfold=N/nfolds
+            start=floor(perfold*fold)
+            end=floor(perfold*(fold+1))
+            idxs=idxs[start:end]
+            testingset[idxs]=True
+        trainingset= ~testingset
+        
+        classifier.train(features[trainingset],labels[trainingset])
+        prediction=classifier.apply(features[testingset])
+        predictions[testingset]=prediction
+        for p, r in zip(prediction,labels[testingset]):
+            cmatrix[r,p] += 1
+
+    if return_predictions:
+        return cmatrix, labelnames, predictions
+    return cmatrix, labelnames
+
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
