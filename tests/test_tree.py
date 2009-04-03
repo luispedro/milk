484b72439d1a6005eeb9fbab8a77bfa55e9d3228
Luís Pedro Coelho <lpc@cmu.edu>
2009-04-03 18:24:54 -0400

    Test tree for >50% accuracy

---
+++ tests/test_tree.py
@@ -0,0 +1,12 @@
+import milk.supervised.tree
+import numpy as np
+import tests.data.german.german
+
+def test_tree():
+    data = tests.data.german.german.load()
+    features = data['data']
+    labels = data['label']
+    C = milk.supervised.tree.tree_classifier()
+    C.train(features,labels)
+    assert ((np.array([C.apply(f) for f in features]) > .5) == labels).mean() > .5
+
