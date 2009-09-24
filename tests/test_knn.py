1e04a0731c65be4f37d0e6bf4bdecf3b04f4dfe2
Luís Pedro Coelho <lpc@cmu.edu>
2009-09-24 13:30:48 -0400

    Nearest neigbour is no more

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -15,3 +15,17 @@ def test_simple():
     assert kNN.apply([0,0,1]) == Y[0]
     assert kNN.apply([0,1,1]) == Y[1]
 
+def test_nnclassifier():
+    labels=[0,1]
+    data=[[0.,0.],[1.,1.]]
+    C = milk.supervised.knn.kNN(1)
+    C.train(data,labels)
+    assert C.apply(data[0]) == 0
+    assert C.apply(data[1]) == 1
+    assert C.apply([.01,.01]) == 0
+    assert C.apply([.99,.99]) == 1
+    assert C.apply([100,100]) == 1
+    assert C.apply([-100,-100]) == 0
+    assert C.apply([.9,.9]) == 1
+    middle = C.apply([.5,.5])
+    assert (middle == 0) or (middle == 1)
