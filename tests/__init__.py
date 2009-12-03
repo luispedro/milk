82b714c702788e5c60a6bfb88d6c5301329e567c
Luís Pedro Coelho <lpc@cmu.edu>
2009-12-03 18:40:21 -0500

    Nicer error message if milksets is not found.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,15 @@
 import glob
 import os.path
+try:
+    import milksets
+    del milksets
+except ImportError:
+    import sys
+    sys.stderr.write('''\
+    Could not import milksets.
+
+    This companion package does not provide any functionality, but
+    is necessary for some of the testing.''')
 basedir = os.path.dirname(__file__)
 for mod in glob.glob(basedir + '/*.py'):
     mod = os.path.basename(mod)
