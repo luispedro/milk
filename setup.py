833e14706638b9b97af3ec197136ea72b9f6c752
Luis Pedro Coelho <lpc@cmu.edu>
2010-04-02 20:23:44 -0400

    More documentation: long description
    Also: update spelling of my name.

--- setup.py
+++ setup.py
@@ -37,9 +37,36 @@ ext_modules = [svm_ext]
 
 packages = filter(lambda p: p.startswith('milk'), setuptools.find_packages())
 
+long_description = '''\
+Milk is a machine learning toolkit in Python. Its focus is on supervised classification.
+
+milk wraps libsvm in a Pythonic way (the models learned have weight arrays that
+are accessible from Python directly, the models are pickle()able, you can pass
+any Python function as a kernel,....)
+
+It also supports k-means clustering with an implementation that is careful not
+to use too much memory (if your dataset fits into memory, milk can cluster it).
+
+It does not have its own file format or in-memory format, which I consider a
+feature as it works on numpy arrays directly (or anything that is convertible to
+a numpy-array) without forcing you to copy memory around. For SVMs, you can even
+just use any datatype if you have your own kernel function.
+
+Features
+--------
+- SVMs. Using the libsvm solver with a pythonesque wrapper around it.
+- Stepwise Discriminant Analysis for feature selection.
+- K-means using as little memory as possible.
+
+License: MIT
+Author: Luis Pedro Coelho (with code from LibSVM)
+Website: `http://luispedro.org/software/milk <http://luispedro.org/software/milk>`_
+'''
+
 setup(name = 'milk',
       version = '0.1-beta-0',
       description = 'Machine Learning Toolkit',
+      long_description = long_description,
       author = u'Luis Pedro Coelho',
       author_email = 'lpc@cmu.edu',
       url = 'http://luispedro.org/software/milk',
