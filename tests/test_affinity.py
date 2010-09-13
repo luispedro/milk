cfc0511b6652af3aa4c7163b6e95d93bf4c92291
Luis Pedro Coelho <lpc@cmu.edu>
2010-09-12 20:06:05 -0400

    Smoke test for affinity propagation

---
+++ tests/test_affinity.py
@@ -0,0 +1,12 @@
+import milk.unsupervised.affinity
+import numpy as np
+def test_affinity():
+    np.random.seed(22)
+    X = np.random.randn(100,10)
+    X[:40] += .4
+    S = milk.unsupervised.pdist(X)
+    clusters, labels = milk.unsupervised.affinity.affinity_propagation(S)
+    assert labels.max()+1 == len(clusters)
+    assert len(labels) = len(X)
+    assert len(labels) == len(X)
+    assert clusters.max() < len(X)
