d7df368d2caa1234abbb676e334625723e61a8db
Luis Pedro Coelho <lpc@cmu.edu>
2010-09-22 13:13:00 -0400

    Fix nfoldcrossvalidation by using integer arithmetic
    Integers instead of floating point: they do not have rounding errors.
    
    Also, made the code simpler, especially the `origins is None` branch
    which is now handled by setting `origins = np.arange(len(labels)) as
    before

--- milk/measures/nfoldcrossvalidation.py
+++ milk/measures/nfoldcrossvalidation.py
@@ -28,9 +28,9 @@ import numpy
 import numpy as np
 
 __all__ = ['foldgenerator', 'getfold', 'nfoldcrossvalidation']
-def foldgenerator(labels, nfolds=None, origins=None, is_ordered=False):
+def foldgenerator(labels, nfolds=None, origins=None):
     '''
-    for train,test in foldgenerator(labels, nfolds=None, origins=None, is_ordered=False)
+    for train,test in foldgenerator(labels, nfolds=None, origins=None)
         ...
 
     This generator breaks up the data into `n` folds (default 10).
@@ -43,98 +43,86 @@ def foldgenerator(labels, nfolds=None, origins=None, is_ordered=False):
 
     Parameters
     ----------
-      labels : the labels
-      nfolds : nr of folds (default 10)
-      origins : if present, must be an array of indices of the same size as labels.
-      is_ordered : whether input is in canonical format: i.e., labels/origins
-                   are 0..N-1/0..K-1, in order. If True, this can be faster
+    labels : a sequence
+        the labels
+    nfolds : integer
+        nr of folds (default 10 or minimum label size)
+    origins : if present, must be an array of indices of the same size as labels.
+
     Returns
     -------
-      iterator over `train, test`, two boolean arrays
+    iterator over `train, test`, two boolean arrays
 
     Bugs
     ----
     Algorithm is very naive w.r.t. unbalanced origins.
-
-    This is slower than it could be if `origins is None`.
     '''
+    labels,_ = normaliselabels(labels)
+    nlabels = labels.max()+1
+    label_locations = [np.where(labels == i)[0] for i in xrange(nlabels)]
+    label_counts = np.array(map(len, label_locations))
+    if origins is not None:
+        origins = np.asanyarray(origins)
+        if len(origins) != len(labels):
+            raise ValueError(
+             'milk.nfoldcrossvalidation.foldgenerator: origins must be of same size as labels')
+        label_counts_orig = np.zeros(nlabels, int)
+        label_origins = [[] for i in xrange(nlabels)]
+        counted = set()
+        for lab, orig in zip(labels, origins):
+            if orig in counted:
+                if orig not in label_origins[lab]:
+                    for other in xrange(nlabels):
+                        if other == lab: continue
+                        if orig in label_origins[other]:
+                            raise ValueError('milk.nfoldcrossvalidation.foldgenerator: origin %s seems to be present in two labels %s and %s' % (orig, lab, other))
+                    raise AssertionError('This should have been unreachable code')
+                continue
+            label_counts_orig[lab] += 1
+            label_origins[lab].append(orig)
+            counted.add(orig)
+        label_origins_counts = [np.cumsum([(origins == orig).sum() for orig in label_origins[lab]])
+                        for lab in xrange(nlabels)]
+    else:
+        label_counts_orig = label_counts
 
-    if origins is None:
-        origins = np.arange(len(labels))
-    assert len(origins) == len(labels), \
-         'milk.nfoldcrossvalidation.foldgenerator: origins must be of same size as labels'
-    counted = set()
-    classcounts = defaultdict(int)
-    for L,orig in zip(labels,origins):
-        if orig in counted: continue
-        classcounts[L] += 1
-        counted.add(orig)
-    min_class_count = min(classcounts.values())
-    if min_class_count == 1:
+    if nfolds is None:
+        nfolds = min(label_counts_orig.min(), 10)
+    elif nfolds > label_counts_orig.min():
+        from warnings import warn
+        warn('milk.measures.nfoldcrossvalidation: Reducing the nr. of folds from %s to %s (minimum class size).' % (nfolds, label_counts_orig.min()))
+        nfolds = label_counts_orig.min()
+    if nfolds == 1:
+        note = ('(taking `origins` into account)' if origins is not None else '')
         raise ValueError('''
 milk.nfoldcrossvalidation.foldgenerator: nfolds was reduced to 1 because minimum class size was 1.
 
-If you passed in an origins parameter, it might be caused by having  a class come from a single origin.
+If you passed in an origins parameter, it might be caused by having a class come from a single origin.
 
-The class histogram (taking origins into account looks like):
-    %s''' % classcounts)
+The class histogram %s looks like:
+%s''' % (note, label_counts_orig))
 
-    if nfolds is None:
-        nfolds = min(10, min_class_count)
-    elif min_class_count < nfolds:
-        from warnings import warn
-        warn('milk.measures.nfoldcrossvalidation: Reducing the nr. of folds to %s (minimum class size).' % min_class_count)
-        nfolds = min_class_count
-
-    if not is_ordered:
-        labels,_ = normaliselabels(labels)
-        reorder = None
-        if np.any(np.diff(labels) < 0):
-            reorder = range(len(labels))
-            reorder.sort(key=(lambda i: (labels[i], origins[i])))
-            reorder = np.array(reorder)
-            labels = labels[reorder]
-            origins = origins[reorder]
-            for i,ri in enumerate(reorder.copy()):
-                reorder[ri] = i
-        origins_ordered = np.empty_like(origins)
-        index = defaultdict(xrange(len(origins)).__iter__().next)
-        for idx,orig in enumerate(origins):
-            origins_ordered[idx] = index[orig]
-        origins = origins_ordered
-        if reorder is not None:
-            for Tr,Te in foldgenerator(labels, nfolds, origins, is_ordered=True):
-                yield Tr[reorder], Te[reorder]
-            return
-
-    nlabels = labels.max() + 1
-    weights = np.zeros(origins.max()+1, np.float)
-    label_origin = np.zeros(origins.max()+1, np.float)
-    origin_labels = [np.where(labels == L)[0] for L in xrange(nlabels)]
-    offset = [origins[labels == L].min() for L in xrange(nlabels)]
-    perfold = 1./nfolds
-    for lab,orig in zip(labels, origins):
-        label_origin[orig] = lab
-        weights[orig] += 1
-
-    for lab in set(labels):
-        is_lab = (label_origin == lab)
-        weights[is_lab] /= (weights * is_lab).sum()
-        weights[is_lab] = np.cumsum(weights[is_lab])
-
-    testing = np.empty(len(labels), np.bool)
+    perfolds = (label_counts // nfolds)
+    assert perfolds.min() > 0
+    testing = np.zeros(len(labels), bool)
+    start = np.zeros(nlabels, int)
     for fold in xrange(nfolds):
         testing.fill(False)
-        for label in xrange(nlabels):
-            start = perfold * fold
-            # We need to force the last fold to be way past the end to avoid rounding errors
-            # missing some elements
-            end = perfold * (fold+1) + (fold == nfolds-1)
-            start,end = np.searchsorted(weights[label_origin == label], (start, end))
-            start += offset[label]
-            end += offset[label]
-            testing |= (origins >= start)&(origins<end)
+        end = start + perfolds
+        # We need to force the last fold to be past the end to avoid rounding errors
+        # missing some elements
+        if fold == (nfolds - 1):
+            end.fill(len(labels))
+        for i,locs,s,e in zip(xrange(nlabels), label_locations, start, end):
+            if origins is None:
+                testing[locs[s:e]] = 1
+            else:
+                s,e = np.searchsorted(label_origins_counts[i], (s,e))
+                for included in label_origins[i][s:e]:
+                    testing[origins == included] = True
         yield ~testing, testing
+        start = end
+
 
 def getfold(labels, fold, nfolds=None, origins=None, is_ordered=False):
     '''
@@ -169,7 +157,7 @@ def nfoldcrossvalidation(features, labels, nfolds=None, classifier=None, origins
       labels : an array of labels, where label[i] is the label corresponding to features[i]
       nfolds : Nr of folds.
       origins : Origin ID (see foldgenerator)
-      
+
       return_predictions : whether to return predictions
 
     classifier should implement the train() method to return a model (something with an apply() method)
