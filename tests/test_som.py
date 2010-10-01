49fd81078d65280dcd807f50bacb54ba38439c72
Luis Pedro Coelho <lpc@cmu.edu>
2010-10-01 11:54:02 -0400

    Compare against slow implementation

--- tests/test_som.py
+++ tests/test_som.py
@@ -1,15 +1,44 @@
 import numpy as np
 import random
-import random
 import milk.unsupervised.som
 
-def test_som():
+
+def _slow_putpoints(grid, points, L=.2):
+    for point in points:
+        dpoint = grid-point
+        y,x = np.unravel_index(np.abs(dpoint).argmin(), dpoint.shape)
+        for dy in xrange(-4, +4):
+            for dx in xrange(-4, +4):
+                ny = y + dy
+                nx = x + dx
+                if ny < 0 or ny >= grid.shape[0]:
+                    continue
+                if nx < 0 or nx >= grid.shape[1]:
+                    continue
+                L2 = L/(1+np.abs(dy)+np.abs(dx))
+                grid[ny,nx] *= 1. - L2
+                grid[ny,nx] += point*L2
+
+
+def data_grid():
+    np.random.seed(22)
     data = np.arange(100000, dtype=np.float32)
-    grid = np.array([data.flat[np.random.randint(0, data.size)] for i in xrange(64*64)]).reshape((64,64))
-    points = data[:100].copy().astype(np.float32).reshape((-1,1))
+    grid = np.array([data.flat[np.random.randint(0, data.size)] for i in xrange(64*64)]).reshape((64,64,1))
+    data = data.reshape((-1,1))
+    return grid, data
+
+def test_som():
+    grid, points = data_grid()
+    points = points[:100]
     grid2 = grid.copy()
     milk.unsupervised.som.putpoints(grid, points, L=0., R=1)
     assert np.all(grid == grid2)
     milk.unsupervised.som.putpoints(grid, points, L=.5, R=1)
     assert not np.all(grid == grid2)
 
+def test_against_slow():
+    grid, points = data_grid()
+    grid2 = grid.copy()
+    milk.unsupervised.som.putpoints(grid, points[:10], shuffle=False)
+    _slow_putpoints(grid2.reshape((64,64)), points[:10])
+    assert np.allclose(grid, grid2)
