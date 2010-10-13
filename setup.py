3879628a5f0b14163b7b0fc2f96ecacf10ff23c1
Luis Pedro Coelho <lpc@cmu.edu>
2010-10-12 20:30:05 -0400

    ENH: README is in rst format
    Read it in setup.py instead of repeating myself

--- setup.py
+++ setup.py
@@ -32,38 +32,13 @@ On linux, the package is often called python-setuptools'''
     exit(1)
 from numpy.distutils.core import setup, Extension
 execfile('milk/milk_version.py')
+long_description = file('README.rst').read()
 
 svm_ext = Extension('milk.supervised._svm', sources = ['milk/supervised/_svm.cpp'])
 som_ext = Extension('milk.unsupervised._som', sources = ['milk/unsupervised/_som.cpp'])
 ext_modules = [svm_ext, som_ext]
 
 packages = filter(lambda p: p.startswith('milk'), setuptools.find_packages())
-
-long_description = '''\
-Milk is a machine learning toolkit in Python. Its focus is on supervised classification.
-
-milk wraps libsvm in a Pythonic way (the models learned have weight arrays that
-are accessible from Python directly, the models are pickle()able, you can pass
-any Python function as a kernel,....)
-
-It also supports k-means clustering with an implementation that is careful not
-to use too much memory (if your dataset fits into memory, milk can cluster it).
-
-It does not have its own file format or in-memory format, which I consider a
-feature as it works on numpy arrays directly (or anything that is convertible to
-a numpy-array) without forcing you to copy memory around. For SVMs, you can even
-just use any datatype if you have your own kernel function.
-
-Features
---------
-- SVMs. Using the libsvm solver with a pythonesque wrapper around it.
-- Stepwise Discriminant Analysis for feature selection.
-- K-means using as little memory as possible.
-
-License: MIT
-Author: Luis Pedro Coelho (with code from LibSVM)
-Website: `http://luispedro.org/software/milk <http://luispedro.org/software/milk>`_
-'''
 
 setup(name = 'milk',
       version = __version__,
