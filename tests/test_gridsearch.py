4b7a78f20d60f001497fb636c63ce97469b31312
Luis Pedro Coelho <lpc@cmu.edu>
2010-10-18 23:56:21 -0400

    TST: Update test to newer interface

--- tests/test_gridsearch.py
+++ tests/test_gridsearch.py
@@ -1,6 +1,6 @@
 import milk.supervised.gridsearch
 import milk.supervised.svm
-import milk.supervised.gridsearch
+from milk.supervised.gridsearch import gridmaximise, _allassignments
 import numpy as np
 
 def test_gridsearch():
@@ -10,7 +10,7 @@ def test_gridsearch():
     features = features[selected]
     labels = labels[selected]
 
-    G = milk.supervised.gridsearch.gridsearch(
+    G = milk.supervised.gridsearch(
             milk.supervised.svm.svm_raw(),
             params={'C':[.01,.1,1.,10.],
                     'kernel':[milk.supervised.svm.rbf_kernel(0.1),milk.supervised.svm.rbf_kernel(1.)]
@@ -22,14 +22,14 @@ test_gridsearch.slow = True
 
 
 def test_all_assignements():
-    assert len(list(milk.supervised.gridsearch._allassignments({'C': [0,1], 'kernel' : ['a','b','c']}))) == 2 * 3
+    assert len(list(_allassignments({'C': [0,1], 'kernel' : ['a','b','c']}))) == 2 * 3
 
 
 
 def test_gridmaximise():
     from milksets.wine import load
     features, labels = load()
-    x = milk.supervised.gridsearch.gridmaximise(milk.supervised.svm_simple(kernel=np.dot, C=2.), features[::2], labels[::2] == 0, {'C' : (0.5,) })
+    x = gridmaximise(milk.supervised.svm_simple(kernel=np.dot, C=2.), features[::2], labels[::2] == 0, {'C' : (0.5,) })
     cval, = x
     assert cval == ('C', .5)
 
