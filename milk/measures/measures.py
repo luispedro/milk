d1daebd83cf5cc25671170bf4f38d87a37d955fe
Luis Pedro Coelho <lpc@cmu.edu>
2010-10-27 11:01:48 -0400

    ENH: 0/1 Loss
    Measures will start moving to this interface: real x predicted -> m.

--- milk/measures/measures.py
+++ milk/measures/measures.py
@@ -1,26 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2008-2010, Luis Pedro Coelho <lpc@cmu.edu>
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 # 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-#  all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#  THE SOFTWARE.
+# License: MIT
 
 from __future__ import division
 import numpy
+import numpy as np
 
 __all__ = ['accuracy','waccuracy']
 
@@ -33,6 +19,25 @@ def accuracy(cmatrix):
     cmatrix = numpy.asanyarray(cmatrix)
     return cmatrix.trace()/cmatrix.sum()
 
+def zero_one_loss(real, predicted):
+    '''
+    loss = zero_one_loss(real, predicted)
+
+    Parameters
+    ----------
+    real : sequence
+        the underlying labels
+    predicted : sequence
+        the predicted labels
+
+    Returns
+    -------
+    loss : integer
+        the number of instances where `real` differs from `predicted`
+    '''
+    return np.sum(np.asanyarray(real) != np.asanyarray(predicted))
+    
+
 def waccuracy(cmatrix):
     '''
     wacc = waccuracy(cmatrix)
@@ -44,5 +49,4 @@ def waccuracy(cmatrix):
 
 ## TODO: Implement http://en.wikipedia.org/wiki/Matthews_Correlation_Coefficient
 
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 
