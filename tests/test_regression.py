bd522da738cff2666e2919541bce21f2758015dc
Luis Pedro Coelho <lpc@cmu.edu>
2010-11-08 16:55:52 -0500

    TST: Test a simple example
    This was reported by Mao Ziyang

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -17,3 +17,19 @@ def test_svm_crash():
     # The line below crashed milk:
     milk.supervised._svm.eval_LIBSVM(X,Y,Alphas0,p,params,kernel,cache_size)
     # HASN'T CRASHED!
+
+
+def test_nov2010():
+    # Bug submitted by Mao Ziyang
+    # This was failing in 0.3.5 because SDA selected no features
+    np.random.seed(222)
+    features = np.random.randn(100,20)
+    features[:50] *= 2
+    labels = np.repeat((0,1), 50)
+
+    classifier = milk.defaultclassifier()
+    model = classifier.train(features, labels)
+    new_label = model.apply(np.random.randn(20)*2)
+    new_label2 = model.apply(np.random.randn(20))
+    assert new_label == 0
+    assert new_label2 == 1
