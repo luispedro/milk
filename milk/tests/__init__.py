425e3065aeffb67aac02ebd8b5f01fe3a161f49f
Luis Pedro Coelho <lpc@cmu.edu>
2011-03-04 21:10:50 -0500

    BLD Add code to run tests from inside milk
    I do not really know what the other code was doing.

--- milk/tests/__init__.py
+++ milk/tests/__init__.py
@@ -1,5 +1,3 @@
-import glob
-import os.path
 try:
     import milksets
     del milksets
@@ -10,14 +8,9 @@ except ImportError:
 
     This companion package does not provide any functionality, but
     is necessary for some of the testing.''')
-basedir = os.path.dirname(__file__)
-for mod in glob.glob(basedir + '/*.py'):
-    mod = os.path.basename(mod)
-    if mod == '__init__.py':
-        continue
-    mod = mod[:-len('.py')]
-    _tmp = __import__(mod, globals(), locals(), [], level=-1)
-    exec('%s = _tmp' % mod)
-del glob
-del os
-del _tmp
+
+
+def run():
+    import nose
+    nose.main()
+
