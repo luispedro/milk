2de738519871fc20a2108cf12d2472db33929d82
Luis Pedro Coelho <luis@luispedro.org>
2011-11-03 10:58:09 -0400

    ENH Make jug nfoldcrossvalitation simpler
    Using jug.identity is a much simpler solution than the previous hack.
    
    The previous implementation did not handle the case in which features is
    a Task correctly, either.

--- milk/ext/jugparallel.py
+++ milk/ext/jugparallel.py
@@ -23,37 +23,9 @@ try:
     from jug import TaskGenerator, value
     from jug.utils import identity
     from jug.mapreduce import mapreduce
+    from jug.mapreduce import reduce as jug_reduce
 except ImportError:
     raise ImportError('milk.ext.jugparallel requires jug (http://luispedro.org/software/jug)')
-
-class _nfold_one(object):
-    def __init__(self, features, labels, kwargs):
-        self.features = features
-        self.labels = labels
-        self.kwargs = kwargs
-
-    def __call__(self, i):
-        return milk.nfoldcrossvalidation(
-                    value(self.features),
-                    value(self.labels),
-                    folds=[i],
-                    **value(self.kwargs))
-
-    def __jug_hash__(self):
-        # jug.hash is only available in jug 0.8
-        # This is also the first version that would call __jug_hash__
-        # So, we import it here only.
-        from jug import hash
-        M = hash.new_hash_object()
-        hash.hash_update(M,[
-            ('type', 'milk.nfoldcrossvalidation'),
-            ('features', self.features),
-            ('labels', self.labels),
-            ('kwargs', self.kwargs),
-            ])
-        hexdigest = M.hexdigest()
-        self.__jug_hash__ = lambda: hexdigest
-        return hexdigest
 
 def _nfold_reduce(a,b):
     cmat = a[0] + b[0]
@@ -91,9 +63,11 @@ def nfoldcrossvalidation(features, labels, **kwargs):
     milk.nfoldcrossvalidation : The same functionality as a "normal" function
     jug.CompoundTask : This function can be used as argument to CompoundTask
     '''
-    mapper = _nfold_one(features, labels, kwargs)
+    features,labels = map(identity, (features,labels))
     nfolds = kwargs.get('nfolds', 10)
-    return mapreduce(_nfold_reduce, mapper, range(nfolds), map_step=1, reduce_step=(nfolds+1))
+    nfold_one = TaskGenerator(milk.nfoldcrossvalidation)
+    mapped = [nfold_one(features, labels, folds=[i], **kwargs) for i in xrange(nfolds)]
+    return jug_reduce(_nfold_reduce, mapped)
 
 
 def _select_min(s0, s1):
