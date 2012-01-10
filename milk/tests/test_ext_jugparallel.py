97f8be03f17ca824a780792033266dd293678169
Luis Pedro Coelho <luis@luispedro.org>
2012-01-10 08:24:43 -0500

    TST Update jug tests to new jug interfaces

--- milk/tests/test_ext_jugparallel.py
+++ milk/tests/test_ext_jugparallel.py
@@ -2,7 +2,7 @@ try:
     import jug
     from jug import value
     import jug.options
-    from jug.tests.task_reset import task_reset
+    from jug.tests.utils import task_reset, simple_execute
 except ImportError:
     from nose import SkipTest
     def task_reset(f):
@@ -13,17 +13,13 @@ except ImportError:
 @task_reset
 def test_nfoldcrossvalidation():
     store, space = jug.jug.init('milk/tests/data/jugparallel_jugfile.py', 'dict_store')
-    options = jug.options.default_options
-    jug.jug.execute(store, options)
+    simple_execute()
     assert len(jug.value(space['classified'])) == 2
     assert len(jug.value(space['classified_wpred'])) ==3
 
 
 @task_reset
 def test_kmeans():
-    from jug.task import alltasks
     store, space = jug.jug.init('milk/tests/data/jugparallel_kmeans_jugfile.py', 'dict_store')
-    options = jug.options.default_options
-
-    jug.jug.execute(store, options)
+    simple_execute()
     assert len(value(space['clustered'])) == 2
