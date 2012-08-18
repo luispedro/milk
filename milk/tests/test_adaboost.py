cb69318a4979dee7aa9307c425eb8e6b93c8b38f
Luis Pedro Coelho <luis@luispedro.org>
2012-08-18 16:21:35 +0100

    TST Test for bug
    This was reported as issue #6 on github by
    Austin Richardson (audy)

--- milk/tests/test_adaboost.py
+++ milk/tests/test_adaboost.py
@@ -8,6 +8,22 @@ def test_learner():
     features = features[labels < 2]
     labels = labels[labels < 2] == 0
     labels = labels.astype(int)
-    model = learner.train(features, labels)
+    model = learner.train(features[::2], labels[::2])
     train_out = np.array(map(model.apply, features))
     assert (train_out == labels).mean() > .9
+
+
+def test_too_many_boolean_indices_regression():
+    import milk.supervised.randomforest
+    import milk.supervised.adaboost
+    import milksets.wine
+    from milk.supervised.multi import one_against_one
+
+    weak = milk.supervised.randomforest.rf_learner()
+    learner = milk.supervised.adaboost.boost_learner(weak)
+    learner = one_against_one(learner)
+
+    features, labels = milksets.wine.load()
+
+    # sample features so that the test is faster (still gives error):
+    learner.train(features[::16], labels[::16])
