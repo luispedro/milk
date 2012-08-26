f7c40ccb4a30486715cf6edcd0b6ee712ee3fccb
Luis Pedro Coelho <luis@luispedro.org>
2012-08-26 13:43:11 +0100

    MIN: Past-proofing: import with_statement

--- milk/utils/parallel.py
+++ milk/utils/parallel.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011, Luis Pedro Coelho <luis@luispedro.org>
+# Copyright (C) 2011-2012, Luis Pedro Coelho <luis@luispedro.org>
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 # License: MIT. See COPYING.MIT file in the milk distribution
 
-from __future__ import division
+from __future__ import division, with_statement
 import multiprocessing
 
 max_procs = 1
