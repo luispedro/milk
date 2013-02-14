3f86b7a5a9242798807d508f070176bc53c712c3
Luis Pedro Coelho <luis@luispedro.org>
2013-02-14 12:35:12 +0100

    BLD Fix distribution of source
    Previously, the distribution was not actually buildable.
    
    I have now been able to build on a Mac OS X.

--- get-eigen.sh
+++ get-eigen.sh
@@ -2,5 +2,6 @@
 wget http://bitbucket.org/eigen/eigen/get/3.1.2.tar.bz2
 tar xjf 3.1.2.tar.bz2
 cd eigen-eigen-5097c01bcdc4
+mkdir -p ../milk/supervised/eigen3
 cp -r Eigen ../milk/supervised/eigen3
 
