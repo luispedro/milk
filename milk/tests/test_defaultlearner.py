52ec9e1afb0db8f9e54b9ae7a65dc8f3c1da325e
Luis Pedro Coelho <luis@luispedro.org>
2014-08-25 11:08:21 +0200

    TST Test empty input to model
    Currently, triggers a failure

--- milk/tests/test_defaultlearner.py
+++ milk/tests/test_defaultlearner.py
@@ -1,3 +1,4 @@
+import numpy as np
 import milk
 def test_extra_arg():
     from milksets.wine import load
@@ -5,3 +6,13 @@ def test_extra_arg():
     learner = milk.defaultlearner()
     model = learner.train(features[::2],labels[::2], extra_arg=5)
     assert model.apply(features[1]) < 12.
+
+
+def test_empty_input():
+    learn = milk.defaultlearner()
+    X = np.random.rand(60, 3)
+    X[:32] += .52
+    y = np.arange(60) > 35
+    model = learn.train(X, y)
+    preds = model.apply_many([])
+    assert len(preds) == 0
